eaea8abb2f7f19b60edcb531e1ec85e73def87e5
Alexei Dayrit <alexeigdayrit@gmail.com>
2023-07-30 21:14:52 -0700

    Added constants folder/file and added links to the footer component.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
 import Image from "next/image";
 
+import { footerLinks } from "@/constants";
+
 const Footer = () => {
   return (
     <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
-      <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 py-10">
+      <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
         <div className="flex flex-col justify-start items-start gap-6">
           <Image
             src="/logo.svg"
@@ -18,7 +20,22 @@ const Footer = () => {
             All rights reserved &copy;
           </p>
 
-
+          <div className="footer__links">
+            {footerLinks.map((link) => (
+              <div key={link.title} className="footer__link">
+                <h3 className="font-bold">{link.title}</h3>
+                {link.links.map((item) => (
+                  <Link
+                    key={item.title}
+                    href={item.url}
+                    className="text-gray-500"
+                  >
+                    {item.title}
+                  </Link>
+                ))}
+              </div>
+            ))}
+          </div>
         </div>
 
       </div>
