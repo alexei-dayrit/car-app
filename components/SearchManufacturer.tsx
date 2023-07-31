6a804b3891d4f6ba61192055b99dd8cadecd7530
Alexei Dayrit <alexeigdayrit@gmail.com>
2023-07-31 16:26:58 -0700

    Installed headlessui for react and updated SearchManufacturer to use Combobox.

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import { SearchManufacturerProps } from '@/types'
+"use client";
+
+import { Combobox, Transition } from '@headlessui/react';
+import { SearchManufacturerProps } from '@/types';
+import Image from 'next/image'
+import { useState } from 'react';
 
 const SearchManufacturer = ({manufacturer, setManufacturer}:
   SearchManufacturerProps) => {
+    const [query, setQuery] = useState('')
+
   return (
-    <div>SearchManufacturer</div>
+    <div className='search-manufacturer'>
+      <Combobox>
+        <div className='relative w-full'>
+          <Combobox.Button className={"absolute top-[14px]"}>
+            <Image
+              src="/car-logo.svg"
+              width={20}
+              height={20}
+              className="ml-4"
+              alt="Car Manufacturer Search Logo"
+            />
+          </Combobox.Button>
+
+          <Combobox.Input
+            className="search-manufacturer__input"
+            placeholder='Volkswagen'
+            displayValue={(manufacturer: string) => manufacturer}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+        </div>
+      </Combobox>
+    </div>
   )
 }
 
