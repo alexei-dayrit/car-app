15b890564a99d4fb625928c21bf1a6607514f33a
Alexei Dayrit <alexeigdayrit@gmail.com>
2023-08-01 15:39:56 -0700

    Added imagin api util function and added key to .env.

--- utils/index.ts
+++ utils/index.ts
@@ -52,5 +52,16 @@ export async function fetchCars() {
 }
 
 export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL('https://cdn.imagin.studio/getimage');
+  const { make, year, model } = car;
+  const imaginAPiKey = process.env.IMAGIN_API_KEY;
 
+  url.searchParams.append('customer', imaginAPiKey || '');
+  url.searchParams.append('make', make);
+  url.searchParams.append('modelFamily', model.split(' ')[0]);
+  url.searchParams.append('zoomType', 'fullscreen');
+  url.searchParams.append('modelYear', `${year}`);
+  url.searchParams.append('angle', `${angle}`);
+
+  return `${url}`;
 }
