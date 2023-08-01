460bb291637fe455889050aabbd4910154a253e0
Alexei Dayrit <alexeigdayrit@gmail.com>
2023-08-01 05:22:11 -0700

    Added rent calculation function in utils folder and styled rental pricing in CarCard component.

--- utils/index.ts
+++ utils/index.ts
@@ -17,6 +17,21 @@ const axios = require('axios');
 // 	console.error(error);
 // }
 
+export const calculateCarRent = (city_mpg: number, year: number) => {
+  const basePricePerDay = 50; // Base rental price per day in dollars
+  const mileageFactor = 0.1; // Additional rate per mile driven
+  const ageFactor = 0.05; // Additional rate per year of vehicle age
+
+  // Calculate additional rate based on mileage and age
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  // Calculate total rental rate per day
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+  return rentalRatePerDay.toFixed(0);
+};
+
 export async function fetchCars() {
   const headers =  {
     'X-RapidAPI-Key': '3d92dcb08emsh6596f846cc6d7c9p1da7f7jsncf1cd34bdc95',
