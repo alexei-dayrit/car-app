f57eef941ed6bbacbc4c638a183424cc6603a2c8
Alexei Dayrit <alexeigdayrit@gmail.com>
2023-08-01 05:11:12 -0700

    Outlined section for cars in home page and added CarCard component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-import { CustomFilter, Hero, SearchBar } from "@/components";
+import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
 import { fetchCars } from "@/utils";
 import Image from "next/image";
 
 export default async function Home() {
   const allCars = await fetchCars();
   console.log('allCars:', allCars)
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
   return (
     <main className="overflow-hidden">
@@ -21,8 +23,26 @@ export default async function Home() {
 
           <div className="home__filter-container">
             <CustomFilter />
+            <CustomFilter />
           </div>
         </div>
+
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => (
+                <CarCard car={car}/>
+              ))}
+            </div>
+          </section>
+          ):  (
+            <div className="home__error-container">
+              <h2 className="text-black text-xl font-bold">Oops, no results</h2>
+              <p>{allCars?.message}</p>
+            </div>
+          )
+        }
+
       </div>
     </main>
   )
