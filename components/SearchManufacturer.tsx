b81146e0e49ef931af23c63c8278f2c3e4f501a1
Alexei Dayrit <alexeigdayrit@gmail.com>
2023-07-31 17:02:40 -0700

    Updating state in SearchBar component via SearchManufacturer ComboBox.

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -20,7 +20,7 @@ const SearchManufacturer = ({manufacturer, setManufacturer}:
 
   return (
     <div className="search-manufacturer">
-      <Combobox>
+      <Combobox value={manufacturer} onChange={setManufacturer}>
         <div className="relative w-full">
           <Combobox.Button className={"absolute top-[14px]"}>
             <Image
