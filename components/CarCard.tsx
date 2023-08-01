df3333e789c381364240d9f059c8018ba2ed97df
Alexei Dayrit <alexeigdayrit@gmail.com>
2023-08-01 05:14:53 -0700

    Added CarCardProps type.

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+"use client";
 
-const CarCard = ({car}: any) => {
-  return (
-    <div>CarCard</div>
-  )
+import { useState } from "react";
+import Image from "next/image";
+import { CarProps } from "@/types";
+
+interface CarCardProps {
+  car: CarProps
 }
+
+const CarCard = ({ car }: CarCardProps) => {
+  return <div>CarCard</div>;
+};
 
 export default CarCard
