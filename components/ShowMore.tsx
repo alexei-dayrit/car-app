f983f99328118b76c10f7db89222397d1067665f
Alexei Dayrit <alexeigdayrit@gmail.com>
2023-08-01 19:38:27 -0700

    Added custom button to ShowMore component which updates the limit in the url.

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+"use client";
 
-const ShowMore = () => {
+import { useRouter } from 'next/navigation'
+import { ShowMoreProps } from '../types/index';
+import CustomButton from './CustomButton';
+import { updateSearchParams } from '@/utils';
+
+const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
+  const router = useRouter();
+
+  const handleNavigation = () => {
+    const newLimit = (pageNumber + 1) * 10;
+    const newPathName = updateSearchParams('limit', `${newLimit}`);
+
+    router.push(newPathName, { scroll: false })
+  }
+
   return (
-    <div>ShowMore</div>
-  )
-}
+    <div className='w-full flex-center gap-5 mt-10'>
+      {!isNext && (
+        <CustomButton
+          title="Show More"
+          btnType='button'
+          containerStyles='bg-primary-blue rounded-full text-white'
+          handleClick={handleNavigation}
+        />
+      )}
+    </div>
+  );
+};
 
 export default ShowMore
