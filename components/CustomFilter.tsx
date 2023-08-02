7c29740b8edc653ebf4f80d8f859a508b0d8659b
Alexei Dayrit <alexeigdayrit@gmail.com>
2023-08-01 19:14:07 -0700

    Added utility function to updateSearchParams.

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -5,15 +5,25 @@ import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { Listbox, Transition } from '@headlessui/react';
 import { CustomFilterProps } from '@/types';
+import { updateSearchParams } from '@/utils';
 
 const CustomFilter = ({ title, options }: CustomFilterProps) => {
-  const [selected, setSelected] = useState(options[0])
+  const router = useRouter();
+  const [selected, setSelected] = useState(options[0]);
+
+  const handleUpdateParams = (e: {title: string, value: string}) => {
+    const newPathName = updateSearchParams(title, e.value.toLowerCase());
+    router.push(newPathName, {scroll: false});
+  }
 
   return (
     <div className='w-fit'>
       <Listbox
         value={selected}
-        onChange={(e) => setSelected(e)}
+        onChange={(e) => {
+          setSelected(e);
+          handleUpdateParams(e);
+        }}
       >
         <div className='relative w-fit z-10'>
           <Listbox.Button className='custom-filter__btn'>
@@ -41,7 +51,8 @@ const CustomFilter = ({ title, options }: CustomFilterProps) => {
                   ${active ? 'bg-primary-blue text-white' : 'text-gray-900'}`}
                 >
                   {({ selected }) => (
-                    <span>
+                    <span className={`block truncate ${selected ? 'font-medium'
+                    : 'font-normal'}`}>
                       {option.title}
                     </span>
                   )}
