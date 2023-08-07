5da1360ed14236c3c3e175cbd12442113d5678b4
Alexei Dayrit <alexeigdayrit@gmail.com>
2023-08-07 14:35:44 -0700

    Added a favicon.

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,6 @@ export default async function Home({ searchParams }: any) {
 
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
-
   return (
     <main className="overflow-hidden">
       <Hero />
